85b3c61d5b965332180f9c3085585d7b36cddf6e
PC-06 <PC-06@DESKTOP-4D0DSA1>
2023-06-01 14:05:24 +0900

    chap11

---
+++ chapter11/src/chap11/sec03/HashMapEx.java
@@ -0,0 +1,27 @@
+package chap11.sec03;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapEx {
+
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<Integer, String> ();
+		//자료 저장
+		map.put(1000, "홍길동");
+		map.put(1001, "이순신");
+		map.put(1002, "강감찬");
+		
+		String name=map.get(1001);
+		System.out.println(name);
+		
+		Set<Integer> keySet=map.keySet();
+		Iterator<Integer> iter=keySet.iterator();
+		while(iter.hasNext()) {
+			Integer keys=iter.next();
+			System.out.println(keys+"=>"+map.get(keys));
+		}
+	}
+}
