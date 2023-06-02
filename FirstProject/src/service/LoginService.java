8986eb3df54d72f6ecbecb4a8f3c3aa3b290078d
PC-06 <PC-06@DESKTOP-4D0DSA1>
2023-06-02 11:50:12 +0900

    db

--- FirstProject/src/service/LoginService.java
+++ FirstProject/src/service/LoginService.java
@@ -7,33 +7,44 @@ import dao.LoginDAO;
 
 public class LoginService {
 	private static LoginService instance;
-	private LoginService() {}
-	public static LoginService getinstance() {
-		if(instance==null) instance=new LoginService();
+
+	private LoginService() {
+	}
+
+	public static LoginService getInstance() {
+		if (instance == null)
+			instance = new LoginService();
 		return instance;
 	}
-	
+
 	Scanner sc = new Scanner(System.in);
-	
-	public static int loginCount=0;
-	LoginDAO dao=LoginDAO.getinstance();
+
+	public static int loginCount = 0;
+	LoginDAO dao = LoginDAO.getInstance();
+	Map<String, Object> result=null;
 	
 	public void login() {
 		System.out.println("[[ 로그인 ]]");
-		
-		System.out.print("id 입력 : ");
-		String id=sc.nextLine();
-		
-		System.out.print("비밀번호 입력 : ");
-		String pass=sc.nextLine();
-		Map<String, Object> result=dao.login(id, pass);
-		
-		loginCount++;
-		
-		if(result != null) {
-			System.out.println(result.get("MEM_NAME")+"님 반갑습니다.");
-		} else {
-			System.out.println("다시 로근이 하세요...");
-		}
+		while (true) {
+			System.out.print("ID 입력 : ");
+			String id = sc.nextLine();
+
+			System.out.print("비밀번호 입력 : ");
+			String pw = sc.nextLine();
+			result = dao.login(id, pw);
+			loginCount++;
+			if(loginCount>2) break;
+			if (result != null) {
+				System.out.println(result.get("MEM_NAME") + "님 반갑습니다.");
+				break;
+			} else {
+				System.out.println("다시로그인 하세요...");
+			}
+		}				
+	}
+	
+	public Map<String, Object> isDuplicate(String id) {
+		result=dao.select(id);
+		return result;
 	}
 }
--- FirstProject/src/util/JDBCUtil.java
+++ FirstProject/src/util/JDBCUtil.java
@@ -13,7 +13,6 @@ import java.util.Map;
 
 //JDBC를 사용하여 CRUD를 효율적으로 사용할 수 있는 메서드를 포함한 class
 public class JDBCUtil {
-	
 //싱글톤패턴 적용
 	private static JDBCUtil instance=null;
 	private JDBCUtil() {}
@@ -22,82 +21,78 @@ public class JDBCUtil {
 		return instance;
 	}
 	
-	private String url ="jdbc:oracle:thin:@localhost:1521:xe";
-	private String user = "pc06";
-	private String passwd = "java";
+	private String url="jdbc:oracle:thin:@localhost:1521:xe";
+	private String user="pc06";
+	private String passwd="java";
 	
 	private Connection conn;
 	private PreparedStatement pstmt=null;
 	private Statement stmt=null;
 	private ResultSet rs=null;
 	
-	public Map<String, Object> selectOne(String sql) {
-		//정적쿼리 사용한 경우 
-		//sql="SELECT * FROM TBL_MEMBER WHERE MEM_ID='a001' AND MEM_PASS='1234'"
-		//sql="SELECT SUM(CART_QTY * PROD_PRICE) 
-		//FROM CART, PROD 
-		//WHERE CART_PROD=PROD_ID 
-		//AND CART_MEMBER=(SELECT A.MEM_ID 
-		//FROM (SELECT MEM_ID FROM MEMBER ORDER BY MEM_MILEAGE DESC A WHERE ROWNUM=1);
+	public Map<String, Object> selectOne(String sql){
+		//정적쿼리 사용한경우
+		//sql="SELECT * FROM TBL_MEMBER WHERE MEM_ID='a001' AND 
+		//      MEM_PASS='1234'"
+	    Map<String, Object> row=null;
+	    try {
+	    	conn=DriverManager.getConnection(url,user,passwd);
+	    	pstmt=conn.prepareStatement(sql);
+	    	rs=pstmt.executeQuery();
+	    	//컬럼의 수, 컬럼명
+	    	ResultSetMetaData rsmd=rs.getMetaData();
+	    	int columnCount=rsmd.getColumnCount();
+	    	while(rs.next()) {
+	    		row=new HashMap<>();
+	    		for(int i=0; i<columnCount; i++) {
+	    			String key=rsmd.getColumnLabel(i);
+	    	//or	String key=rsmd.getColumnName(i);
+	    			Object value=rs.getObject(i);
+	    			row.put(key, value);
+	    		}
+	    	}
+	    }catch(SQLException e) {
+	    	e.printStackTrace();
+	    }finally {
+	    	if(rs!=null) try{rs.close();}catch(Exception e) {}
+	    	if(pstmt!=null) try{pstmt.close();}catch(Exception e) {}
+	    	if(conn!=null) try{conn.close();}catch(Exception e) {}    	
+	    }
+	    return row;
+	}
+	
+	public Map<String, Object> selectOne(String sql, List<Object>param){
+		//sql="SELECT * FROM tbl_member WHERE mem_id = ? and
+		//  mem_pass = ? " 
 		Map<String, Object> row=null;
 		try {
-			
 			conn=DriverManager.getConnection(url,user,passwd);
 			pstmt=conn.prepareStatement(sql);
-			rs=pstmt.executeQuery();
-			
-			// 컬럼의 수, 컬럼명
-			ResultSetMetaData rsmd=rs.getMetaData(); 
-			int columnCount=rsmd.getColumnCount();
-			while(rs.next()) {
-				row=new HashMap<>();
-				for(int i=0; i<columnCount; i++) {
-					String key=rsmd.getColumnLabel(i);
-			//or 	String key=rsmd.getColumnName(i);
-					Object value=rs.getObject(i);
-					row.put(key, value);
-				}
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if(rs!=null) try{rs.close();}catch (Exception e) {}
-			if(pstmt!=null) try{pstmt.close();}catch (Exception e) {}
-			if(conn!=null) try{conn.close();}catch (Exception e) {}
-		}
-		return row;
-	}
-	public Map<String, Object> selectOne(String sql, List<Object>param) {
-		//sql="SELECT * FROM tbl_member WHERE mem_id = ? and
-		//									  mem_pass = ?"
-		Map<String, Object> row=null;
-		try {
-			conn=DriverManager.getConnection(url, user, passwd);
-			pstmt=conn.prepareStatement(sql);
-			//sql에 존재하는 ?에 대응되는 데이터 mapping=>pstmt.set타입명(?순번, 데이터)
+			//sql에 존재하는  ?에 대응되는 데이터 mapping=>
+			//    pstmt.set타입명(?순번,데이터) 
 			for(int i=0; i<param.size(); i++) {
 				pstmt.setObject(i+1, param.get(i));
 			}
 			rs=pstmt.executeQuery();
 			
 			ResultSetMetaData rsmd=rs.getMetaData();
-			int columneCount=rsmd.getColumnCount();
+			int columnCount=rsmd.getColumnCount();
 			while(rs.next()) {
 				row=new HashMap<>();
-				for(int i=0; i<columneCount; i++) {
-					String key=rsmd.getColumnLabel(i);
-					Object value=rs.getObject(i);
+				for(int i=0; i<columnCount; i++) {
+					String key=rsmd.getColumnLabel(i+1);
+					Object value=rs.getObject(i+1);
 					row.put(key, value);
 				}
 			}
-		} catch (SQLException e) {
+		}catch(SQLException e) {
 			e.printStackTrace();
-		} finally {
-			if(rs!=null) try{rs.close();}catch (Exception e) {}
-			if(pstmt!=null) try{pstmt.close();}catch (Exception e) {}
-			if(conn!=null) try{conn.close();}catch (Exception e) {}
+		}finally {
+	    	if(rs!=null) try{rs.close();}catch(Exception e) {}
+	    	if(pstmt!=null) try{pstmt.close();}catch(Exception e) {}
+	    	if(conn!=null) try{conn.close();}catch(Exception e) {}    				
 		}
+
 		return row;
 	}
 }
